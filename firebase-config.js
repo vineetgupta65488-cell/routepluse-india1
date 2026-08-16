e80246e86a6b4d5a2d119326e9add33f6ffc4b99
vineetgupta65488-cell <vineet.gupta65488@gmail.com>
2026-08-16 16:46:58 +0530

    Fix Firebase Web API key and config to match Firebase console

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,11 @@
 // RoutePulse India Firebase Web App configuration
 // This web configuration is safe to use in the client-side website.
 window.firebaseConfig = {
-  apiKey: "AIzaSyCmFjqnJmFHJTCeuiFwZpbnd8kGV9tAcg",
+  apiKey: "AIzaSyCmFjqnJmFHJICeuiuEwZpbnd8kGV9tAqc",
   authDomain: "routepulse-india.firebaseapp.com",
   projectId: "routepulse-india",
   storageBucket: "routepulse-india.firebasestorage.app",
   messagingSenderId: "474571170677",
-  appId: "1:474571170677:web:d152c6eb30727b1782293c"
+  appId: "1:474571170677:web:d152c6eb30727b1782293c",
+  measurementId: "G-TBZ22V7ZGL"
 };
